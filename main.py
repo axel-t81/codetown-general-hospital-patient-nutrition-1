4cde5b56422cbe791779e1ffb135cc6240d2c753
Axel <63882492+axel-t81@users.noreply.github.com>
2024-07-31 15:34:13 +1000

    Update main.py
    Need to use SO answer to both fix other valid input tests, and also read the referred to guide on a better way of handling this input validation test

--- main.py
+++ main.py
@@ -22,7 +22,14 @@ def main():
     print("How many patients are you collecting data for, today?")
     patients_string = input()
     # input() collects values as strings, so typecast to int
-    patients = int(patients_string)
+    # Using the float() function avoids the 'Value Error' when their is an 'invalid literal' entered in the input, eg a decimal
+    patients = int(float(patients_string))
+
+    while patients < 1:
+        print("Sorry, the number of patients must be a positive number. Please try again.")
+        print("How many patients are you collecting data for, today?")
+        patients_string = input()
+        patients = int(float(patients_string))
 
     # Requirement 2: "loop this number of times to collect the amount of protein, carbohydrates, and fat required for each patient."
     # Using range() allows the fort loop to be interable over an int
@@ -30,26 +37,64 @@ def main():
         # For UI and user-friendliness, add 1 to zero-based index and typecast int x to string
         x_string = str(x + 1)
 
-        print("How much protein is required for patient " + x_string + "?")
+        print("How many grams of protein is required for patient " + x_string + "?")
         protein_string = input()
-        protein = int(protein_string)
+        protein = float(protein_string)
+        while protein < 0.0:
+            print("Sorry, the amount must be a non-negative number. Please try again.")
+            print("How many grams of protein is required for patient " + x_string + "?")
+            protein_string = input()
+            protein = float(protein_string)
         # Append most recent entry to end of appropriate List
-        all_protein.append(protein)
+        all_protein.append(int(protein))
 
-        print("How many carbohydrates are required for patient " + x_string + "?")
+        print("How many grams of carbohydrates are required for patient " + x_string + "?")
         carbs_string = input()
-        carbs = int(carbs_string)
+        carbs = int(float(carbs_string))
+        while carbs < 0.0:
+            print("Sorry, the amount must be a non-negative number. Please try again.")
+            print("How many grams of carbohydrates are required for patient " + x_string + "?")
+            carbs_string = input()
+            carbs = int(float(carbs_string))
         all_carbs.append(carbs)
 
-        print("How much fat is required for patient " + x_string + "?")
+        print("How many grams of fat is required for patient " + x_string + "?")
         fat_string = input()
-        fat = int(fat_string)
+        fat = int(float(fat_string))
+        while fat < 0:
+            print("Sorry, the amount must be a non-negative number. Please try again.")
+            print("How many grams of fat are required for patient " + x_string + "?")
+            fat_string = input()
+            fat = int(float(fat_string))
         all_fat.append(fat)
 
-        # Print functions for testing and understanding by programmer
-        print(all_protein)
-        print(all_carbs)
-        print(all_fat)
+        # Calculate Kjs for each patient, and add it to the appropriate List
+        kilojoules_unrounded = (4.18 * (4 * protein + 4 * carbs + 9.30 * fat))
+        kilojoules = round(kilojoules_unrounded, 4)
+        all_kjs.append(kilojoules)
+
+    ## Print functions for testing and understanding by programmer
+    ## print(all_protein)
+    ## print(all_carbs)
+    ## print(all_fat)
+    ## print(all_kjs)
+
+    # Calculate and Output Averages of All Four Lists
+    protein_average_float = round(sum(all_protein) / len(all_protein),2)
+    protein_average = str(protein_average_float)
+    print("The average protein for all patients is " + protein_average + " grams.")
+
+    carbs_average_float = round(sum(all_carbs) / len(all_carbs),2)
+    carbs_average = str(carbs_average_float)
+    print("The average carbohydrates for all patients is " + carbs_average + " grams.")
+
+    fat_average_float = round(sum(all_fat) / len(all_fat),2)
+    fat_average = str(fat_average_float)
+    print("The average fat for all patients is " + fat_average + " grams.")
+
+    kjs_average_float = round(sum(all_kjs) / len(all_kjs),2)
+    kjs_average = str(kjs_average_float)
+    print("The average kilojoules for all patients is " + kjs_average + " kJs.")
 
 
 if __name__ == "__main__":
