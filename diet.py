25aba34363cb0d701c6022f23b2d3a5481a15681
Axel <63882492+axel-t81@users.noreply.github.com>
2024-08-02 14:28:31 +1000

    Update diet.py
    (1) Added function and updated section to account for patient number (2) Made cosmetic changes to improve UI.

--- diet.py
+++ diet.py
@@ -9,21 +9,39 @@ __version__ = "0.1.0"
 __license__ = "None. All Rights Reserved."
 
 # A function to collect valid input for macro nutritional data
-# This function validates input to confirm (a) it is an integer, and (b) that it is non-negative.
+# This function validates input to confirm (a) it is a number that fits the requirements of a float, and (b) that it is non-negative.
 # For citing sources, this function is from learning from StackOverflow
 # My original StackOverflow question was asked here: https://stackoverflow.com/questions/78814504/in-python-why-is-this-negative-float-passing-the-non-negative-while-loop-test-c
 # And not only did @Andrew Yim answer my original question, he pointed me to a StackFlow guide on how to better tackle this input validation problem.
 # The guide he pointed me to was here: https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
-def non_negative_int_only(prompt):
+# For learning quickly about Try and Except, this guide was used: https://www.youtube.com/watch?v=MImAiZIzzd4
+def non_negative_only(prompt):
     while True:
         try:
             value = float(input(prompt))
         except:
-            print("Sorry, I didn't understand your input. Let's try again, using a whole integer or number with decimal places, as input please.")
+            print("Sorry, I didn't understand your input. Let's try again, using a whole number or one with decimal places, as your input please.")
             continue
 
         if value < 0:
             print("Sorry, the amount must be a non-negative number. Please try again.")
+            continue
+        else:
+            break
+    return value
+
+# A function to collect valid input for patients numbers
+# This function validates input to confirm (a) it is an integer to count people, and (b) that it is positive and above zero.
+def positive_int_only(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+        except:
+            print("Sorry, I didn't understand your input. Let's try again, using a whole number to count people, as your input please.")
+            continue
+
+        if value < 1:
+            print("Sorry, the number of patients must be a positive number. Please try again.")
             continue
         else:
             break
@@ -39,32 +57,28 @@ all_kjs = []
 # Guarded main function, as per https://www.python-boilerplate.com/py3+executable
 # A guarded main function prevents the program being run when it is imported elsewhere?
 def main():
+    # For UI and user-friendliness, provide feedback to user of program start/entry point.
+    print("\nWELCOME TO THE CODETOWN GENERAL HOSPITAL PATIENT NUTRITION SYSTEM:\n")
     # Requirement 1: "request the number of patient's dietary requirements that will be entered in the interaction with the program"
-    patients_string = input("\nHow many patients are you collecting data for, today? ")
-    # input() collects values as strings, so typecast to int
-    # Using the float() function avoids the 'Value Error' when their is an 'invalid literal' entered in the input, eg a decimal
-    patients = int(float(patients_string))
+    patients = positive_int_only("\nHow many patients are you collecting data for, today? ")
 
-    while patients < 1:
-        print("Sorry, the number of patients must be a positive number. Please try again.")
-        print("How many patients are you collecting data for, today?")
-        patients_string = input()
-        patients = int(float(patients_string))
 
+    # For UI and user-friendliness, provide feedback and guidance to user of where they are in the program.
+    print("\nSTEP 1 of 2 - PATIENT NUTRITION COLLECTION:")
     # Requirement 2: "loop this number of times to collect the amount of protein, carbohydrates, and fat required for each patient."
     # Using range() allows the fort loop to be interable over an int
     for x in range(patients):
         # For UI and user-friendliness, when describing patient to user, add 1 to zero-based index and typecast int x to string
         x_string = str(x + 1)
 
-        protein = non_negative_int_only("\nHow many grams of protein is required for patient " + x_string + "? ")
+        protein = non_negative_only("\nHow many grams of protein is required for patient " + x_string + "? ")
         # Append most recent entry to end of appropriate List.
         all_protein.append(protein)
 
-        carbs = non_negative_int_only("\nHow many grams of carbohydrates are required for patient " + x_string + "? ")
+        carbs = non_negative_only("\nHow many grams of carbohydrates are required for patient " + x_string + "? ")
         all_carbs.append(carbs)
 
-        fat = non_negative_int_only("\nHow many grams of fat is required for patient " + x_string + "? ")
+        fat = non_negative_only("\nHow many grams of fat is required for patient " + x_string + "? ")
         all_fat.append(fat)
 
         # Calculate Kjs for each patient, and add it to the appropriate List
@@ -79,7 +93,7 @@ def main():
     ## print(all_kjs)
 
     # Calculate and Output Averages of All Four Lists
-    print("\nCALCULATED PATIENT INFORMATION:")
+    print("\nSTEP 2 of 2 - CALCULATED PATIENT INFORMATION:\n")
     protein_average_float = round(sum(all_protein) / len(all_protein),2)
     protein_average = str(protein_average_float)
     print("The average protein for all patients is " + protein_average + " grams.")
@@ -95,6 +109,8 @@ def main():
     kjs_average_float = round(sum(all_kjs) / len(all_kjs),2)
     kjs_average = str(kjs_average_float)
     print("The average kilojoules for all patients is " + kjs_average + " kJs.\n")
+
+    print("\nTHE SYSTEM IS NOW COMPLETE: YOUR PATIENTS' NUTRITIONAL INFORMATION IS NOW AVAILABLE FOR USE.\nPLEASE CLOSE THIS PROGRAM OR RUN AGAIN. THANK YOU.\n")
 
 
 if __name__ == "__main__":
